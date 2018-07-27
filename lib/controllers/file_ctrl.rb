e4684d60e3b6834eb91bed53d6b384ba8f138b74
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2018-07-27 11:16:41 -0300

    Added api to recover xml files to xls

--- lib/controllers/file_ctrl.rb
+++ lib/controllers/file_ctrl.rb
@@ -33,7 +33,7 @@ module Controllers
             begin
               copy file[:tempfile], File.new(DI_FILE_DIR + '/' + file[:filename], 'w+')
 
-              FileScheduler.new(File.open(DI_FILE_DIR + '/' + file[:filename])){ |file|
+              FileScheduler.new(File.open(DI_FILE_DIR + '/' + file[:filename])) {|file|
                 extend ReadParseXML
                 di_array = parse_xml(file)
                 extend WriteXLS
@@ -56,6 +56,35 @@ module Controllers
               ModelException.new("Error on Upload", 400, 400, e).to_response
             end
           }
+
+
+          c.get('/consulta') {
+
+            di_array = []
+
+            extend ReadFiles
+            files = discover_di_files(Dir.pwd + '/dis/')
+            extend ReadParseXML
+
+            files.each do |f|
+              di_array = parse_xml(f)
+            end
+
+            extend WriteXLS
+
+            date_time = DateTime.now.to_s
+            date_time.gsub!(":", '-')
+
+            output_file_path = Dir.pwd + "/output/relatorio.dis.#{date_time}.xlsx"
+
+            save_xls_file_report(di_array, output_file_path)
+
+            temp = File.new(output_file_path)
+            send_file temp.path, :filename => output_file_path, :type => "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+
+
+          }
+
         }
       end
     end
